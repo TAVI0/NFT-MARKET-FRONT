e90d73512cc05418abd3c316553342498c4ed8df
TAVI0 <marcos_tavio@outlook.com>
2022-09-28 01:52:07 -0300

    checkpoint

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
-import Home from './views/home/index.js';
-import Web3 from 'web3';
-import { useEffect } from 'react';
+import Home from './views/home';
+import MainLayout from './layouts/main';
+import Punks from './views/punks';
 
 function App() {
 
-  useEffect(() => {
-    if (window.ethereum) {
-      // window.ethereum
-      //   .request({
-      //     method: "eth_requestAccounts",
-      //   })
-      //   .then((accounts) => console.log(accounts));
-
-      const web3 = new Web3(window.ethereum);
-      web3.eth.requestAccounts().then(console.log);
-    }
-  }, []);
-
-
   return (
-    <>
+    <MainLayout>
     <Routes>
-      <Route path="/" element={<Home />}/>
+     <Route path="/" exact element={<Home/>} />
+     <Route path="/punks" exact element={<Punks/>} />
     </Routes>
-    </>
-  );
+   </MainLayout>
+);
 }
 
 export default App;
